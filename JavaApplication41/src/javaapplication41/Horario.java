b86efde4caf44e18dfa57f0584a9e5817a1bf44a
Javiera Cabrera <jcabrera16@alumnos.utalca.cl>
2020-04-04 17:26:29 -0300

    Clase Horario

--- JavaApplication41/src/javaapplication41/Horario.java
+++ JavaApplication41/src/javaapplication41/Horario.java
@@ -10,8 +10,18 @@ package javaapplication41;
  * @author 56992
  */
 public class Horario {
+    public String inicio;
+    public String fin;
+    public int horas;
     
+    public Horario(String inicio, String fin){
+        String[] datosInicio=null;
+        String[] datosFin=null;
+        datosInicio=inicio.split(":");
+        datosFin=inicio.split(":");
+        horas=calculoHoras(Integer.parseInt(datosInicio[0]),Integer.parseInt(datosFin[0]));            
+    }
     public static int calculoHoras(int i, int f){
-        return 0;
+        return i-f;
     }
 }
